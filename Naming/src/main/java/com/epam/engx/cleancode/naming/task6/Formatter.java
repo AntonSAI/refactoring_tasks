4247a07a14beabd77d8f653d749f49a3cbcf57e2
Anton Salva <antonsforw@gmail.com>
2020-03-09 21:59:15 +0300

    ref

--- Naming/src/main/java/com/epam/engx/cleancode/naming/task6/Formatter.java
+++ Naming/src/main/java/com/epam/engx/cleancode/naming/task6/Formatter.java
@@ -2,22 +2,22 @@ package com.epam.engx.cleancode.naming.task6;
 
 public class Formatter {
 
-    private static final String PLUS = "+";
-    private static final String PIPE = "|";
-    private static final String MINUS = "-";
-    private static final String UNDERSCORE = " _ ";
+    private static final String FORMAT_FOR_PLUS = "+";
+    private static final String FORMAT_FOR_PIPE = "|";
+    private static final String FORMAT_FOR_MINUS = "-";
+    private static final String FORMAT_FOR_UNDERSCORE = " _ ";
 
     public static void main(String[] args) {
-        System.out.println(formatKyeValue("enable", "true"));
-        System.out.println(formatKyeValue("name", "Bob"));
+        System.out.println(formatKeyValue("enable", "true"));
+        System.out.println(formatKeyValue("name", "Bob"));
     }
 
-    private static String formatKyeValue(String key, String value) {
-        String content = key + UNDERSCORE + value;
-        String minuses = repeat(MINUS, content.length());
-        return PLUS +  minuses + PLUS + "\n"
-                + PIPE + content + PIPE + "\n" +
-                PLUS + minuses + PLUS + "\n";
+    private static String formatKeyValue(String key, String value) {
+        String content = key + FORMAT_FOR_UNDERSCORE + value;
+        String minusRepeat = repeat(FORMAT_FOR_MINUS, content.length());
+        return FORMAT_FOR_PLUS +  minusRepeat + FORMAT_FOR_PLUS + "\n"
+                + FORMAT_FOR_PIPE + content + FORMAT_FOR_PIPE + "\n" +
+                FORMAT_FOR_PLUS + minusRepeat + FORMAT_FOR_PLUS + "\n";
     }
 
     private static String repeat(String symbol, int times) {
