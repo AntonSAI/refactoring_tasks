985d2beeff3eed6a44eee45d6261c8d561d36156
Anton Salva <antonsforw@gmail.com>
2020-03-10 10:45:19 +0300

    first fix

--- Naming/src/main/java/com/epam/engx/cleancode/naming/task1/CollectOrderService.java
+++ Naming/src/main/java/com/epam/engx/cleancode/naming/task1/CollectOrderService.java
@@ -10,12 +10,14 @@ public class CollectOrderService implements OrderService {
 
     private CollectionService collectionService;
     private NotificationManager notificationManager;
+    private final String INFO_NOTIFICATION_LEVEL = 4;
+    private final String CRITICAL_NOTIFICATION_LEVEL = 1;
 
     public void submitOrder(Order order) {
         if (collectionService.isEligibleForCollection(order))
-            notificationManager.notifyCustomer(Message.READY_FOR_COLLECT, 4); // 4 - info notification level
+            notificationManager.notifyCustomer(Message.READY_FOR_COLLECT, INFO_NOTIFICATION_LEVEL); // 4 - info notification level
         else
-            notificationManager.notifyCustomer(Message.IMPOSSIBLE_TO_COLLECT, 1); // 1 - critical notification level
+            notificationManager.notifyCustomer(Message.IMPOSSIBLE_TO_COLLECT, CRITICAL_NOTIFICATION_LEVEL); // 1 - critical notification level
     }
 
     public void setCollectionService(CollectionService collectionService) {
--- Naming/src/main/java/com/epam/engx/cleancode/naming/task6/Formatter.java
+++ Naming/src/main/java/com/epam/engx/cleancode/naming/task6/Formatter.java
@@ -2,10 +2,10 @@ package com.epam.engx.cleancode.naming.task6;
 
 public class Formatter {
 
-    private static final String FORMAT_FOR_PLUS = "+";
-    private static final String FORMAT_FOR_PIPE = "|";
-    private static final String FORMAT_FOR_MINUS = "-";
-    private static final String FORMAT_FOR_UNDERSCORE = " _ ";
+    private static final String PLUS = "+";
+    private static final String PIPE = "|";
+    private static final String MINUS = "-";
+    private static final String UNDERSCORE = " _ ";
 
     public static void main(String[] args) {
         System.out.println(formatKeyValue("enable", "true"));
@@ -13,11 +13,11 @@ public class Formatter {
     }
 
     private static String formatKeyValue(String key, String value) {
-        String content = key + FORMAT_FOR_UNDERSCORE + value;
-        String minusRepeat = repeat(FORMAT_FOR_MINUS, content.length());
-        return FORMAT_FOR_PLUS +  minusRepeat + FORMAT_FOR_PLUS + "\n"
-                + FORMAT_FOR_PIPE + content + FORMAT_FOR_PIPE + "\n" +
-                FORMAT_FOR_PLUS + minusRepeat + FORMAT_FOR_PLUS + "\n";
+        String content = key + UNDERSCORE + value;
+        String minusRepeat = repeat(MINUS, content.length());
+        return PLUS +  minusRepeat + PLUS + "\n"
+                + PIPE + content + PIPE + "\n" +
+                PLUS + minusRepeat + PLUS + "\n";
     }
 
     private static String repeat(String symbol, int times) {
